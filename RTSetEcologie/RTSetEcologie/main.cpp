a7a686af5fd746c91a945e2488f222cfea7fa253
loufenrir <loufenrir6@gmail.com>
2026-09-18 11:48:03 +0200

    Verhulst et Lotka-Volterra V1

--- RTSetEcologie/RTSetEcologie/main.cpp
+++ RTSetEcologie/RTSetEcologie/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 
 double populationSuivante(double P, double a, double b, double dt)
 {
@@ -9,6 +10,10 @@ void simuler(double P, double a, double b, double dt, double tempsMax)
 {
     double temps = 0.0;
 
+    /*fichier csv*/
+    std::ofstream fichier("population.csv");
+    fichier << "Temps;Population\n";
+
     while (temps <= tempsMax)
     {
         std::cout << temps
@@ -16,23 +21,65 @@ void simuler(double P, double a, double b, double dt, double tempsMax)
             << P
             << std::endl;
 
+        fichier << temps << ";" << P << "\n";
+
         P = populationSuivante(P, a, b, dt);
 
         temps += dt;
     }
+    fichier.close();
 }
 
+void populationSuivanteLotka(
+    double H,
+    double C,
+    double alpha,
+    double beta,
+    double gamma,
+    double delta,
+    double dt,
+    double& nouveauH,
+    double& nouveauC)
+{
+    nouveauH = H + dt * (alpha * H - beta * H * C);
+
+    nouveauC = C + dt * (-gamma * C + delta * C * H);
+}
+
+
+//Le saint main
 int main()
 {
-    double P = 2.0;
+    double H = 50.0;
+    double C = 100.0;
 
-    double a = 0.05;
-    double b = 0.01;
+    double alpha = 0.05;
+    double beta = 0.001;
+    double gamma = 0.05;
+    double delta = 0.001;
 
     double dt = 1.0;
-    double tempsMax = 100.0;
 
-    simuler(P, a, b, dt, tempsMax);
+    double nouveauH;
+    double nouveauC;
+
+    populationSuivanteLotka(
+        H,
+        C,
+        alpha,
+        beta,
+        gamma,
+        delta,
+        dt,
+        nouveauH,
+        nouveauC
+    );
+
+    std::cout << "H actuel : " << H << std::endl;
+    std::cout << "C actuel : " << C << std::endl;
+
+    std::cout << "H suivant : " << nouveauH << std::endl;
+    std::cout << "C suivant : " << nouveauC << std::endl;
 
     return 0;
 }
