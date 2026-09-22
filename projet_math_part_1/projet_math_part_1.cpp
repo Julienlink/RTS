ffc3fcde631e0c623116bb0076fa167b4e2baf3d
mayorie <mattsachamahe@gmail.com>
2026-09-22 23:23:06 +0200

    base_part_1

--- projet_math_part_1/projet_math_part_1.cpp
+++ projet_math_part_1/projet_math_part_1.cpp
@@ -2,10 +2,77 @@
 //
 
 #include <iostream>
+#include <cmath>
+#include <string>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	//initialisation des variables
+	std::cout << "Choix des valeurs : \n";
+
+	std::cout << "Population initiale des proies lapins_depart (lapins) : ";
+	double lapins_depart;
+	std::cin >> lapins_depart;
+
+	std::cout << "Population initiale des predateurs renards_depart : ";
+	double renards_depart;
+	std::cin >> renards_depart;
+
+	std::cout << "Taux de reproduction des proies (lapins_taux_croissance) : ";
+	double lapins_taux_croissance;
+	std::cin >> lapins_taux_croissance;
+
+	std::cout << "Taux de reproduction des predateurs (renards_taux_croissance) : ";
+	double renards_taux_croissance;
+	std::cin >> renards_taux_croissance;
+
+	std::cout << "Taux de mortalite des proies due aux predateurs (lapins_taux_mortalite) : ";
+	double lapins_taux_mortalite;
+	std::cin >> lapins_taux_mortalite;
+
+	std::cout << "Taux de mortalite naturelle des predateurs (ranards_taux_mortalite) : ";
+	double ranards_taux_mortalite;
+	std::cin >> ranards_taux_mortalite;
+
+	std::cout << "Temps d'observation (0 = manuelle) (val = t) : \n";
+	int temps_observation;
+	std::cin >> temps_observation;
+	int temps = 0;
+	std::cout << "Temps d'observation : " << temps++ << " population de lapins : " << lapins_depart << "\n\n";
+	std::cin.ignore();
+
+
+	double population = lapins_depart;
+
+	double equilibre = lapins_taux_croissance / lapins_taux_mortalite;
+
+	if (temps_observation == 0)
+	{
+		bool flag = true;
+
+		while (flag)
+		{
+			// Équation : P(t+1) = P(t) + aP(t) - bP(t)²
+			population = population + lapins_taux_croissance * population
+				- lapins_taux_mortalite * population * population;
+
+			std::cout << "Temps d'observation : " << temps++
+				<< " population de lapins : " << population << "\n";
+
+			std::cout << "états d’équilibre : " << equilibre << "\n";
+
+			std::cout << "Appuyez sur Entree pour continuer ou entrez 0 pour arreter : \n";
+
+			std::string choix;
+			std::getline(std::cin, choix);
+
+			if (choix == "0")
+			{
+				flag = false;
+			}
+		}
+	}
+
 }
 
 // Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
